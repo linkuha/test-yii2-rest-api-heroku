22694a0f01c9caaf4a08af6fee936ff845f2a4c2
linkuha <linkuha@gmail.com>
2017-09-11 22:09:16 +0700

    deployment changes

--- post-install-cmd.sh
+++ post-install-cmd.sh
@@ -2,6 +2,6 @@
 if [ -n "$DYNO" ]  && [ -n "$ENV" ]; then
     php init --env=$ENV --overwrite=All
     php yii migrate/up --interactive=0
-    php yii cache/flush-all
-    php yii cache/flush-schema --interactive=0
+    # php yii cache/flush-all
+    # php yii cache/flush-schema --interactive=0
 fi
