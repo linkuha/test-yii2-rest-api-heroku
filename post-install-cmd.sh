91292c96bc58c0f64a0784f8a582e35ab4dba4f5
linkuha <linkuha@gmail.com>
2017-09-11 16:24:01 +0700

    environments changes

--- post-install-cmd.sh
+++ post-install-cmd.sh
@@ -3,6 +3,8 @@
 if [ -n "$DYNO" ]
 then
     php init --env=Heroku --overwrite=All
-    ln -s /app/backend/web frontend/web/backend
-    ln -s /app/vendor/bower-asset vendor/bower
+    ln -s /api/web api/web
+    ln -s /frontend/web frontend/web
+    ln -s /backend/web backend/web
+    ln -s /vendor/bower-asset vendor/bower
 fi
